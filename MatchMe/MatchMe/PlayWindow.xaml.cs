82d5e705ad427cae8fc21a582b0940688b145886
Son Phan <sonphanusa@gmail.com>
2014-03-17 16:23:24 -0400

    Create the official project MatchMe
    move the testing MatchingGame project to "old" folder

---
+++ MatchMe/MatchMe/PlayWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MatchMe
+{
+    /// <summary>
+    /// Interaction logic for PlayWindow.xaml
+    /// </summary>
+    public partial class PlayWindow : Window
+    {
+
+    }
+}
